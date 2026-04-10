4bedb260235ba4ffe814ff7a7629ae4b44f08d6a
hkdskavikeshawa <hkdskavikeshawa@students.nsbm.ac.lk>
2026-04-10 21:47:23 +0530

    feat: add database models - User, Project, Match, ResearchArea, SupervisorPreference

---
+++ BlindMatchAPI/Models/ResearchArea.cs
@@ -0,0 +1,10 @@
+﻿namespace BlindMatchAPI.Models
+{
+    public class ResearchArea
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public ICollection<Project> Projects { get; set; } = new List<Project>();
+        public ICollection<SupervisorPreference> SupervisorPreferences { get; set; } = new List<SupervisorPreference>();
+    }
+}
